53d0bc2f888b6f899944456971243d34b7207252
Diya Goyal <dgoyal6_be22@thapar.edu>
2025-03-31 10:31:39 +0530

    variables and datatypes

--- javaBasics/src/Main.java
+++ javaBasics/src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        //variables
+        String name1= "diya";
+        int age1= 20;
+        String neighbour= "isha";
+        String friend= neighbour;
+
+        //PRIMITIVE DATA TYPES
+        //Byte- 1 [-128 to 127]
+        byte age=21;
+        //short-2
+        //int-4
+        int phone= 1234567890;
+        //long-8
+        long phone2= 123456789999L; //L denotes a long type variable
+        //float-4
+        float pi= 3.14F; //F denotes float
+        //double-8
+        //char-2
+        char letter= 'A';
+        //boolean-1
+        boolean isAdult = true;
+
+        //NON PRIMITIVE DATA TYPES
+        //String
+        String name= "ankita";
+        System.out.println(name.length());
+        String roomie= new String("akanksha");
+        System.out.println(roomie);
+
+
     }
 }
+//methods are the functions written in classes
+//println gives a line space after printing the output but if we write only print no space is given
